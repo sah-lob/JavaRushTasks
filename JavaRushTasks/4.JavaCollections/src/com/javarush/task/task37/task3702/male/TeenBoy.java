2902c6d05ddcd1209545c6b6e61602946ee3a1b6
Alexander Lobachev <Sah-lob@ya.ru>
2018-11-14 13:48:51 +0300

    Фабрики (1)

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3702/male/TeenBoy.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+import java.util.StringJoiner;
+
+public class TeenBoy implements Human {
+
+    public static final int MAX_AGE = 19;
+
+    @Override
+    public String toString() {
+        return "TeenBoy{}";
+    }
+}
